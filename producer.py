a0c2d0bb263b756d888ac8786151ea39b966117b
Misikir <mikireyob@gmail.com>
2020-06-04 16:07:53 -0400

    use timestamp within json data instead of utc conversion
    Instead of trying to convert local time to utc to determine
    whether a measurement is with a time interval, use the timestamp
    from the first record in the first json page.
    
    Clean up code by pulling timestamp conversion and json writing
    into separate functions

--- producer.py
+++ producer.py
@@ -17,44 +17,66 @@ class Producer():
         self.http_client = APIClient(project, endpoint)
         self.interval = interval
         self.start_time = None
-    
+
     def fetch_records(self, size):
         """
         Fetches sensor measurements for 'self.interval' time units.
         :param size: number of records to be fetched in one page
         """
         page = 1
-        self.start_time = datetime.utcnow().timestamp()
         response = self.http_client.fetch(size, page)
+        if self.http_client.is_valid(response):
+            self.start_time = response.json()["data"][0]["timestamp"]
+            self.start_time = self.to_timestamp(self.start_time,
+                                                "%Y-%m-%dT%H:%M:%S")
 
-        while self.http_client.is_valid(response) and self.is_in_interval(response):
+        while self.http_client.is_valid(response) and self.is_in_interval(
+                response):
             print(response.status_code)
-            with open('page_{}.json'.format(page), 'w') as fhandle:
-                json.dump(response.json(), fhandle)
+            self.write_json(response.json(), page)
             page += 1
             response = self.http_client.fetch(size, page)
-    
+        print(response.status_code)
+
     def is_in_interval(self, response):
         """
         Checks if the first record in responses.json()
         """
-        if self.start_time is not None:
+        if self.start_time is None:
             raise TypeError("start_time cannot be of None type")
 
         in_interval = False
 
         latest_record_time = response.json()["data"][0]["timestamp"]
-        latest_record_time = datetime.strptime(latest_record_time, "%Y-%m-%dT%H:%M:%S")
-        latest_record_time = latest_record_time.timestamp()
+        latest_record_time = self.to_timestamp(latest_record_time,
+                                               "%Y-%m-%dT%H:%M:%S")
 
         if self.start_time - latest_record_time < self.interval:
             in_interval = True
 
         return in_interval
 
+    def write_json(self, json_obj, page):
+        """
+        Write a json object to file.
+        """
+        with open('{}_page_{}.json'.format(self.start_time, page),
+                  'w') as fhandle:
+            json.dump(json_obj, fhandle)
+
+    @staticmethod
+    def to_timestamp(t, t_format=None):
+        """
+        Converts time from datetime object or a string to time stamp.
+        :param t: time reading to be converted into timestamp. Type should be datetime or str
+        :param t_format: optional for str type time format
+        """
+        if isinstance(t, datetime):
+            return t.timestamp()
+        elif isinstance(t, str):
+            return datetime.strptime(t, t_format).timestamp()
+
+
 if __name__ == "__main__":
     producer = Producer('chicago', 'observations', 300)
     producer.fetch_records(300)
-
-
-    
