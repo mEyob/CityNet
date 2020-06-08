078fb6229d1ca8705ea497f8365bd8c97a60fabc
mEyob <mikireyob@gmail.com>
2020-06-08 02:54:58 +0000

    add kafka producer module

--- producer.py
+++ producer.py
@@ -5,78 +5,94 @@ related to another end point) and puts it in kafka
 """
 
 from api_client import APIClient
-from datetime import datetime
+from kafka import KafkaProducer
 import json
+
+# Number of records in one page of a multi-page api call,
+PAGE_SIZE = 200
+
+DEFAULT_PRODUCER_CONFIG = {
+    "bootstrap_servers": ['10.0.1.16:9092'],
+    "value_serializer": lambda val: json.dumps(val).encode('utf-8')
+}
 
 
 class Producer():
-    def __init__(self, project, endpoint, interval):
+    def __init__(self,
+                 topic,
+                 project,
+                 endpoint,
+                 interval,
+                 config,
+                 page_size=PAGE_SIZE):
         """
-        Create an API client for a specific project and endpoint
+        A wrapper class to a Kafka producer. 
+        Creates an API client for a specific project and endpoint
+        as the source of data and initializes a producer to 
+        channel the fetched data into the system.
+        :param topic: a Kafka topic
+        :param project: the project name to be passed to the APIClient class
+        :param endpoint: API endpoint to be passed to APIClient. Possible 
+        values: ['projects', 'nodes', 'sensors', 'observations']. See 
+        https://arrayofthings.docs.apiary.io/#reference/0/nodes/list-projects
+        :param interval: the length of time (in seconds) covered in the api call
+        :param config: a dictionary of kafka producer configuration
+        :param page_size: number of records in a single page of api call
         """
-        self.http_client = APIClient(project, endpoint)
-        self.interval = interval
-        self.start_time = None
+        self.topic = topic
+        self.api = APIClient(project, endpoint, interval)
+        self.config = config
+        self.page_size = page_size
 
-    def fetch_records(self, size):
-        """
-        Fetches sensor measurements for 'self.interval' time units.
-        :param size: number of records to be fetched in one page
-        """
-        page = 1
-        response = self.http_client.fetch(size, page)
-        if self.http_client.is_valid(response):
-            self.start_time = response.json()["data"][0]["timestamp"]
-            self.start_time = self.to_timestamp(self.start_time,
-                                                "%Y-%m-%dT%H:%M:%S")
+        self.producer = None
 
-        while self.http_client.is_valid(response) and self.is_in_interval(
-                response):
-            print(response.status_code)
-            self.write_json(response.json(), page)
-            page += 1
-            response = self.http_client.fetch(size, page)
-        print(response.status_code)
-
-    def is_in_interval(self, response):
+    def fetch_data(self):
         """
-        Checks if the first record in responses.json()
+        Fetch sensor data in json format using an API client
         """
-        if self.start_time is None:
-            raise TypeError("start_time cannot be of None type")
+        records = None
+        try:
+            records = self.api.fetch_records(self.page_size)
+        except Exception as ex:
+            print("Exception encountered while trying to fetch records")
+            print(str(ex))
+        return records
 
-        in_interval = False
-
-        latest_record_time = response.json()["data"][0]["timestamp"]
-        latest_record_time = self.to_timestamp(latest_record_time,
-                                               "%Y-%m-%dT%H:%M:%S")
-
-        if self.start_time - latest_record_time < self.interval:
-            in_interval = True
-
-        return in_interval
-
-    def write_json(self, json_obj, page):
+    def connect_kafka(self):
         """
-        Write a json object to file.
+        Make a Kafka connection as a producer.
         """
-        with open('{}_page_{}.json'.format(self.start_time, page),
-                  'w') as fhandle:
-            json.dump(json_obj, fhandle)
+        try:
+            self.producer = KafkaProducer(**self.config)
+        except Exception as ex:
+            print("Exception encountered while trying to connect to Kafka")
+            print(str(ex))
 
-    @staticmethod
-    def to_timestamp(t, t_format=None):
+    def publish(self, record):
         """
-        Converts time from datetime object or a string to time stamp.
-        :param t: time reading to be converted into timestamp. Type should be datetime or str
-        :param t_format: optional for str type time format
+        Publish messages to a kafka topic
         """
-        if isinstance(t, datetime):
-            return t.timestamp()
-        elif isinstance(t, str):
-            return datetime.strptime(t, t_format).timestamp()
+        try:
+            self.producer.send(self.topic, value=record)
+            self.producer.flush()
+        except Exception as ex:
+            print("Exception encountered while publishing message")
+            print(str(ex))
+
+    def publish_records(self):
+        """
+        Make a kafka connection as a producer and publish a list 
+        of records. Each record is a json formatted sensor data.
+        """
+        records = self.fetch_data()
+        self.connect_kafka()
+
+        if (records is not None) and (self.producer is not None):
+            for record in records:
+                self.publish(record)
 
 
 if __name__ == "__main__":
-    producer = Producer('chicago', 'observations', 300)
-    producer.fetch_records(300)
+    producer = Producer('observations', 'chicago', 'observations', 300,
+                        DEFAULT_PRODUCER_CONFIG)
+    producer.publish_records()
