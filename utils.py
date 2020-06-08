63a4e8591871e2025a40920911f556873b3ffa2c
mEyob <mikireyob@gmail.com>
2020-06-08 00:54:51 +0000

    add utility functions for interacting with the api
    These were static methods in the Producer class in producer.py
    but it makes more sense to separate them into their own module

---
+++ utils.py
@@ -0,0 +1,39 @@
+"""
+--utils.py--
+Utility functions for APIClient and Producer classes
+"""
+
+from datetime import datetime
+import json
+
+
+def write_json(start_time, json_obj, page):
+    """
+    Write a json object to file.
+    """
+    with open('{}_page_{}.json'.format(start_time, page), 'w') as fhandle:
+        json.dump(json_obj, fhandle)
+
+
+def to_timestamp(t, t_format=None):
+    """
+    Converts time from datetime object or a string to time stamp.
+    :param t: time reading to be converted into timestamp. Type should be datetime or str
+    :param t_format: optional for str type time format
+    """
+    if isinstance(t, datetime):
+        return t.timestamp()
+    elif isinstance(t, str):
+        return datetime.strptime(t, t_format).timestamp()
+
+
+def is_valid(response):
+    """
+    Validate an http request response. Checks the status code 
+    and the length of the 'data' field to determine validity.
+    :param response: a response object to be validated
+    """
+    valid = False
+    if response.status_code == 200 and len(response.json()["data"]) != 0:
+        valid = True
+    return valid
