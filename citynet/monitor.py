4d6f34ee59646887c91c941229837734e3d0566a
Ubuntu <ubuntu@ip-10-0-0-220.ec2.internal>
2020-06-29 16:43:54 +0000

    change duration to absolute timing and make module runnable

--- citynet/monitor.py
+++ citynet/monitor.py
@@ -6,11 +6,16 @@ outliers for a given sensor device.
 """
 
 import sys
+import os
 import argparse
 import psycopg2
 import numpy as np
 from datetime import datetime
 from tabulate import tabulate
+
+sys.path.append(os.path.join(os.path.dirname(__file__), os.pardir))
+
+# citynet imports
 from citynet.consumer import Consumer
 from citynet.utils import to_timestamp
 from citynet.constants import DEFAULT_CONSUMER_CONFIG, DEFAULT_MONITORING_PERIOD, CONNECTION
@@ -29,11 +34,12 @@ class Monitor(Consumer):
         start = None
         records = []
         ts_format = "%Y-%m-%dT%H:%M:%S"
+        start = datetime.utcnow().timestamp()
         for message in self._consumer:
-            if start is None:
-                start = to_timestamp(message.value[0]["timestamp"], ts_format)
             for record in message.value:
                 latest = to_timestamp(record.get("timestamp"), ts_format)
+                if latest < start:
+                    continue
                 if latest - start < duration:
                     if record.get("sensor_path").lower() == self.device_name:
                         records.append(record)
@@ -47,6 +53,7 @@ class Monitor(Consumer):
                             "end": latest
                         }
                     }
+
     def from_db(self, duration):
         """
         Fetch sensor data from database.
@@ -87,8 +94,9 @@ class Monitor(Consumer):
 
         if source is None or source == "live":
             data = self.ingest_records(duration)
-            data = data.get("records")
-            data = list(map(lambda x: float(x.get("value")), data))
+            if data:
+                data = data.get("records")
+                data = list(map(lambda x: float(x.get("value")), data))
         elif source == "historical":
             data = self.from_db(duration)
 
@@ -100,14 +108,14 @@ class Monitor(Consumer):
         time = datetime.now()
         time_str = time.strftime("%Y-%m-%dT%H:%M:%S")
         return {
-                "Device: ": self.device_name,
-                "Current time: ": time_str,
-                "Monitoring duration: ": str(duration) + ' seconds',
-                "Num. of observations: ": num_of_observations,
-                "Mean: ": stat.get("mean", "No data"),
-                "Standard dev: ": stat.get("std", "No data"),
-                "Percentiles: ": stat.get("percentiles", ["No data"] * 4),
-                "Outliers: ": outliers
+            "Device:": self.device_name,
+            "Current time:": time_str,
+            "Monitoring duration:": str(duration) + ' seconds',
+            "Num. of observations:": num_of_observations,
+            "Mean:": stat.get("mean", "No data"),
+            "Standard dev:": stat.get("std", "No data"),
+            "Percentiles:": stat.get("percentiles", ["No data"] * 4),
+            "Outliers:": outliers
         }
 
     @staticmethod
@@ -116,16 +124,16 @@ class Monitor(Consumer):
         Display key statistics in table format
         """
         print("\n**** SUMMARY TABLE ****")
-        stat_table = [["Device", data.get("device")],
-                      ["Current time", data.get("current_time")],
-                      ["Monitoring duration", data.get("monitoring_duration")],
-                      ["Num of observ.", data.get("num of observ")],
-                      ["Mean",data.get("mean")],
-                      ["STD", data.get("std")],
-                      ["25th", data.get("percentiles")[0]],
-                      ["50th", data.get("percentiles")[1]],
-                      ["75th", data.get("percentiles")[2]],
-                      ["95th", data.get("percentiles")[3]]]
+        stat_table = [["Device", data.get("Device:")],
+                      ["Current time", data.get("Current time:")],
+                      ["Monitoring duration", data.get("Monitoring duration:")], 
+                      ["Num of observ.", data.get("Num. of observations:")],
+                      ["Mean", data.get("Mean:")],
+                      ["STD", data.get("Standard dev:")],
+                      ["25th", data.get("Percentiles:")[0]],
+                      ["50th", data.get("Percentiles:")[1]],
+                      ["75th", data.get("Percentiles:")[2]],
+                      ["95th", data.get("Percentiles:")[3]]]
 
         print(tabulate(stat_table, tablefmt="grid"))
 
@@ -135,7 +143,7 @@ class Monitor(Consumer):
             print(tabulate(outliers, ["Outliers"], tablefmt="grid"))
         else:
             print("\n**** NO OUTLIERS DETECTED FOR DEVICE {}! ****\n".format(
-                data.get("device")))
+                data.get("Device:")))
 
     @staticmethod
     def stats(data):
