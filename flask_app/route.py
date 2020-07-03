e9e420c8b716e73dc9f8f2864811d2d556a4614e
Ubuntu <ubuntu@ip-10-0-0-220.ec2.internal>
2020-07-03 04:52:25 +0000

    add sensor histogram and google map plotting functionality

--- flask_app/route.py
+++ flask_app/route.py
@@ -5,13 +5,13 @@ import json
 import numpy as np
 import plotly.graph_objs as go
 
-from flask import make_response
+from flask import make_response, request, render_template
 from datetime import datetime
 
 sys.path.append(os.path.join(os.path.dirname(__file__), os.pardir))
 
 from flask_app import device_monitor, app
-from flask import request, jsonify, render_template
+from flask_googlemaps import Map
 
 
 @app.route('/')
@@ -40,26 +40,44 @@ def monitor():
     num_observations = [stat["num_of_observations"] for stat in stats]
     outliers = [stat["outlier_count"] for stat in stats]
 
-    plot = create_plot(names, num_observations, outliers)
+    plot = outlier_plot(names, num_observations, outliers)
 
     return render_template("index.html", meta=meta, stats=stats, plot=plot)
 
 
-@app.route('/sensor_detail', methods=['GET', 'POST'])
-def sensor_detail():
-    pass
+@app.route('/detail', methods=['GET', 'POST'])
+def detail():
+    """
+    Method for retreiving and ploting sensor-specific 
+    location and histogram.
+    """
+    plot = None
+    sensorloc = None
+    device = request.form.get("device")
+    duration = request.form.get("duration")
+
+    data = device_monitor.from_db(3600, device)
+    data = data.get(device)
+
+    sensor_location = device_monitor.get_location(device)
+    if device:
+        plot = hist_plot(device, data)
+        sensor_location = sensor_location[0]
+        lng, lat = float(sensor_location[0]), float(sensor_location[1])
+
+        sensorloc = Map(identifier="view-side",
+                        lat=lat,
+                        lng=lng,
+                        markers=[(lat, lng)])
+    return render_template("detail.html", plot=plot, sensorloc=sensorloc)
 
 
-def chart(stats, device_name, url):
-    x = range(len(stats))
-    fig, ax = plt.subplots()
-    x_label = ['25%', '50%', 'Mean', '75th', '95th']
-    sns.barplot(x=x_label, y=stats, palette="rocket", ax=ax)
-    ax.set_ylabel(device_name)
-    plt.savefig(url)
-
-
-def create_plot(sensor_names, observations, outliers):
+def outlier_plot(sensor_names, observations, outliers):
+    """
+    Generates a scatter plot of sensors vs observations with 
+    the size of points indicating the number of outlier 
+    observations.
+    """
     min_size = 10
     max_size = 50
 
@@ -86,15 +104,43 @@ def create_plot(sensor_names, observations, outliers):
         width=1000,
         height=600,
         yaxis=dict(title_text="Number of readings",
-                   titlefont=dict(size=18),
+                   titlefont=dict(size=16),
                    showgrid=False,
                    zeroline=False),
-        xaxis=dict(showgrid=False, zeroline=False),
+        xaxis=dict(title_text="Sensor name",
+                   titlefont=dict(size=16),
+                   showgrid=False,
+                   zeroline=False),
         plot_bgcolor="rgb(179,226,205)",
     )
 
     graphJSON = json.dumps(data, cls=plotly.utils.PlotlyJSONEncoder)
 
+    return graphJSON
+
+
+def hist_plot(device_name, data):
+    """
+    Given a sensor, plot a histogram of observations.
+    """
+    fig = go.Figure()
+
+    data = fig.add_trace(
+        go.Histogram(x=data,
+                     name=device_name,
+                     marker_color='#330C73',
+                     opacity=0.75))
+
+    fig.update_layout(yaxis=dict(title_text="Count",
+                                 titlefont=dict(size=16),
+                                 showgrid=False,
+                                 zeroline=False),
+                      xaxis=dict(title_text=device_name,
+                                 showgrid=False,
+                                 zeroline=False),
+                      plot_bgcolor="rgb(179,226,205)",
+                      bargap=0.05)
+    graphJSON = json.dumps(data, cls=plotly.utils.PlotlyJSONEncoder)
     return graphJSON
 
 
